8b7729066f6a48d0d2be88c354be4ee653279aad
Carl Frederik Grønvald <bonezaux@gmail.com>
2018-11-15 14:27:22 +0100

    Add filter.py

--- Main script project1.py
+++ Main script project1.py
@@ -11,48 +11,7 @@ import os
 from dataload import dataLoad
 from dataStatistics import dataStatistics
 from dataPlot import dataPlot
-
-#Apparently how you check whether something is a float in python.
-def isFloat(string):
-    try:
-        float(string)
-    except ValueError:
-        return False;
-    return True;
-
-def filteredData(data, filterData, filterActive):
-    """
-    Filters data according to active filters without changing the data array.
-    """
-    if not filterActive:
-        return data
-    result = data
-    
-    if(len(filterData["Bacteria"]) > 0):
-        #Remove all data and add filtered bacteria back in one at a time
-        result = np.array([[0,0,0]])
-        for bac in filterData["Bacteria"]:
-            result = np.concatenate((result, data[data[:,2]==(bac+1)]), axis=0)
-        result = result[1:,:]
-        
-    if(filterData["Growth"] != None):
-        #select only rows with growth rate btwn max and min
-        result = result[result[:,1] <= filterData["Growth"][1]]
-        result = result[result[:,1] >= filterData["Growth"][0]]
-    
-    return result
-
-def printFilter(filterData, filterActive, bacteriaNames):
-    """
-    Prints current filter, if any, based on given filter data. Needs the bacteria names to print correctly.
-    """
-    if filterActive:
-            print("Current filter:")
-            if(len(filterData["Bacteria"]) > 0):
-                print("Bacteria: " + (", ".join(bacteriaNames[filterData["Bacteria"]])))
-            if(filterData["Growth"] != None):
-                print(str(filterData["Growth"][0]) + " <= Growth rate <= " + str(filterData["Growth"][1]))
-        
+import filter
 
 def MainScript():
     data = np.array([])
@@ -62,13 +21,13 @@ def MainScript():
     
     while True:
         print("""
-              1. Load data
-              2. Filter data
-              3. Display statistics
-              4. Generate plots
-              5. Quit 
+1. Load data
+2. Filter data
+3. Display statistics
+4. Generate plots
+5. Quit 
               """)
-        printFilter(filterData, filterActive, bacteriaNames)
+        filter.printFilter(filterData, filterActive, bacteriaNames)
         userinput = input("Choose a menu point by entering the number: ")
         
         if userinput == "1":
@@ -84,82 +43,10 @@ def MainScript():
                 print("Filename is not valid, check spelling and try again")
                     
         elif userinput == "2":
-            print(""" Filter menu
-           1. Set or add bacteria type to filter
-           2. Set growth rate range filter
-           3. Remove bacteria filter
-           4. Remove growth rate filter
-           5. Remove all filters""")
-            fnum = input("Choose a menu point: ")
-            if(not fnum.isdigit()):
-                print("Write a number!")
-                continue
+            filterTmp = filter.filterMenu(filterData, filterActive, bacteriaNames)
+            filterData = filterTmp[0]
+            filterActive = filterTmp[1]
             
-            if fnum == "1":
-                #Print the bacteria names that are possible to choose
-                print("\n".join([str(x+1)+". "+bacteriaNames[x] for x in range(4) if not x in filterData["Bacteria"]]))
-                #Get a bacteria number
-                bacteria = input("Pick a bacteria number to filter by: ")
-            
-                #Ensure that a valid number between 1 and 4 is provided
-                if not bacteria.isdigit() or int(bacteria) < 1 or int(bacteria) > 4 or int(bacteria)-1 in filterData["Bacteria"]:
-                    print("You need to write a valid bacteria number.")
-                else:
-                    #Subtracting one to match array indices
-                    bacteria = int(bacteria)-1
-                    print("Filtering by " + bacteriaNames[bacteria])
-                    filterData["Bacteria"] += [bacteria]
-                    filterActive=True
-                
-            elif fnum == "2":
-                #Sets growth rate filter
-                minimum = None
-                maximum = None
-                minimum = input("Insert a number for the minimum growth rate: ")
-                if (not isFloat(minimum)) or float(minimum) < 0 or float(minimum) > 1:
-                    print("You need to write a number between 0 and 1.")
-                else:
-                    maximum = input("Insert a number for the maximum growth rate: ")
-                    if (not isFloat(maximum)) or float(maximum) < 0 or float(maximum) > 1:
-                        print("You need to write a number between 0 and 1.")
-                
-                #Checking if the minimum is less than maximum
-                if minimum != None and maximum !=None:
-                    if not float(minimum) <= float(maximum):
-                        print("Invalid range. Minimum larger than or equal to maximum.")
-                    else:
-                        filterData["Growth"] = (float(minimum), float(maximum))
-                        filterActive=True
-                        
-            elif fnum == "3":
-                #Removes bacteria filter
-                filterData["Bacteria"] = []
-                print("Bacteria filter has been removed")
-                
-                #If no growth rate filter,there are no active filters
-                if filterData["Growth"]==None:
-                    filterActive=False
-                    
-            elif fnum == "4":
-                #Removes growth range filter
-                filterData["Growth"] = None
-                print("Growth rate filter have been removed")
-               
-                #If no bacteria filter, there are no active filters
-                if filterData["Bacteria"]==[]:
-                    filterActive=False
-                    
-            elif fnum == "5":
-                #Remove all filters
-                filterData["Growth"] = None
-                filterData["Bacteria"] = []
-                #No active filters
-                filterActive=False
-                print("All filters have been reset")
-            
-            else:
-                print("That is not a valid menu point.")
-                    
         elif userinput == "3":
             #Checking if user remembered to load in data first
             if len(data) == 0:
@@ -180,17 +67,19 @@ def MainScript():
                     if (not Statistics.isdigit()) or int(Statistics) < 1 or int(Statistics) > 7:
                         Statistics = False
                         print("Write an integer between one and seven.")
-                stats = dataStatistics(filteredData(data, filterData, filterActive), Statistics)
+                stats = dataStatistics(filter.filteredData(data, filterData, filterActive), Statistics)
                 if(stats != None):
                     print(options[int(Statistics)-1] + " is " + str(round(stats,3)))
+                    filter.printFilter(filterData, filterActive, bacteriaNames)
+                    input("Press enter to continue ")
                 
         elif userinput == "4":
             #Checking if user remembered to load in data first
             if len(data) == 0:
                 print("\n" + "You must first load data.")
             else:
-                dataPlot(filteredData(data, filterData, filterActive))
-                printFilter(filterData, filterActive, bacteriaNames)
+                dataPlot(filter.filteredData(data, filterData, filterActive))
+                filter.printFilter(filterData, filterActive, bacteriaNames)
                 input("Press enter to continue ")
         
         elif userinput == "5":
---
+++ filter.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 15 14:18:54 2018
+
+@author: carl
+"""
+import numpy as np
+
+#Apparently how you check whether something is a float in python.
+def isFloat(string):
+    try:
+        float(string)
+    except ValueError:
+        return False;
+    return True;
+
+def filteredData(data, filterData, filterActive):
+    """
+    Filters data according to active filters without changing the data array.
+    """
+    if not filterActive:
+        return data
+    result = data
+    
+    if(len(filterData["Bacteria"]) > 0):
+        #Remove all data and add filtered bacteria back in one at a time
+        result = np.array([[0,0,0]])
+        for bac in filterData["Bacteria"]:
+            result = np.concatenate((result, data[data[:,2]==(bac+1)]), axis=0)
+        result = result[1:,:]
+        
+    if(filterData["Growth"] != None):
+        #select only rows with growth rate btwn max and min
+        result = result[result[:,1] <= filterData["Growth"][1]]
+        result = result[result[:,1] >= filterData["Growth"][0]]
+    
+    return result
+
+def printFilter(filterData, filterActive, bacteriaNames):
+    """
+    Prints current filter, if any, based on given filter data. Needs the bacteria names to print correctly.
+    """
+    if filterActive:
+            print("Current filter:")
+            if(len(filterData["Bacteria"]) > 0):
+                print("Bacteria: " + (", ".join(bacteriaNames[filterData["Bacteria"]])))
+            if(filterData["Growth"] != None):
+                print(str(filterData["Growth"][0]) + " <= Growth rate <= " + str(filterData["Growth"][1]))
+
+def filterMenu(filterData, filterActive, bacteriaNames):
+    print(""" Filter menu
+1. Set or add bacteria type to filter
+2. Set growth rate range filter
+3. Remove bacteria filter
+4. Remove growth rate filter
+5. Remove all filters""")
+    fnum = input("Choose a menu point: ")
+    
+    if(not fnum.isdigit()):
+        print("Write a number!")
+        return (filterData, filterActive)
+    
+    if fnum == "1":
+        #Print the bacteria names that are possible to choose
+        print("\n".join([str(x+1)+". "+bacteriaNames[x] for x in range(4) if not x in filterData["Bacteria"]]))
+        #Get a bacteria number
+        bacteria = input("Pick a bacteria number to filter by: ")
+    
+        #Ensure that a valid number between 1 and 4 is provided
+        if not bacteria.isdigit() or int(bacteria) < 1 or int(bacteria) > 4 or int(bacteria)-1 in filterData["Bacteria"]:
+            print("You need to write a valid bacteria number.")
+        else:
+            #Subtracting one to match array indices
+            bacteria = int(bacteria)-1
+            print("Filtering by " + bacteriaNames[bacteria])
+            filterData["Bacteria"] += [bacteria]
+            filterActive=True
+        
+    elif fnum == "2":
+        #Sets growth rate filter
+        minimum = None
+        maximum = None
+        minimum = input("Insert a number for the minimum growth rate: ")
+        if (not isFloat(minimum)) or float(minimum) < 0 or float(minimum) > 1:
+            print("You need to write a number between 0 and 1.")
+        else:
+            maximum = input("Insert a number for the maximum growth rate: ")
+            if (not isFloat(maximum)) or float(maximum) < 0 or float(maximum) > 1:
+                print("You need to write a number between 0 and 1.")
+        
+        #Checking if the minimum is less than maximum
+        if minimum != None and maximum !=None:
+            if not float(minimum) <= float(maximum):
+                print("Invalid range. Minimum larger than or equal to maximum.")
+            else:
+                filterData["Growth"] = (float(minimum), float(maximum))
+                filterActive=True
+                
+    elif fnum == "3":
+        #Removes bacteria filter
+        filterData["Bacteria"] = []
+        print("Bacteria filter has been removed")
+        
+        #If no growth rate filter,there are no active filters
+        if filterData["Growth"]==None:
+            filterActive=False
+            
+    elif fnum == "4":
+        #Removes growth range filter
+        filterData["Growth"] = None
+        print("Growth rate filter have been removed")
+       
+        #If no bacteria filter, there are no active filters
+        if filterData["Bacteria"]==[]:
+            filterActive=False
+            
+    elif fnum == "5":
+        #Remove all filters
+        filterData["Growth"] = None
+        filterData["Bacteria"] = []
+        #No active filters
+        filterActive=False
+        print("All filters have been reset")
+    
+    else:
+        print("That is not a valid menu point.")
+    
+    return (filterData, filterActive)
